8fd468f0ad8746569f8e81fd668c73c31558c59f
Livin R <liviin651@gmail.com>
2022-06-25 17:22:38 +0530

    add

--- js/action.js
+++ js/action.js
@@ -6,3 +6,17 @@ for(let i = 0; i<menuLength; i++){
         menuItem[i].className = "menu_active"
     }
 }
+
+ $(document).ready(function(){
+            $('.sidebar_Btn button').click(function(){
+                $('.section_sidebar').toggleClass('active')
+                $('.sidebar_Btn').toggleClass('btn_active')
+                $('.sidebar_Btn span').toggleClass('change')
+            })
+            function removeAllActiveClasses() {
+                $('.section_sidebar ').removeClass('active')
+                $('.sidebar_Btn').removeClass('btn_active')
+                $('.sidebar_Btn span').removeClass('change')
+            }
+            $(window).scroll(removeAllActiveClasses);
+})
